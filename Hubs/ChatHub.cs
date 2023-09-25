1f85d50c526b50118c3d6439be870312d56f9a98
OG-Enoch <144890721+vEnoch24@users.noreply.github.com>
2023-09-25 16:05:58 +0100

    Initial commit

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string username, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", username, message);
+        }
+
+        public async Task SendNotifcation(string message)
+        {
+            await Clients.All.SendAsync("ReceiveNotification", message);
+        }
+
+        public async Task SendMessageToUser(string userId, string message)
+        {
+            await Clients.User(userId).SendAsync("ReceiveMsg", userId, message);
+        }
+    }
+}
